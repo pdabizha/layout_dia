c3f631bd824c203f3d3d7adc16f75946d38ed7d4
Polina Dabizha <polinkadabi29@gmail.com>
2024-11-06 18:12:32 +0200

    first deploy

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,33 @@
 'use strict';
+
+// document.addEventListener('DOMContentLoaded', function() {
+//   const slides = [
+//     '../images/slider/slider-img-1.jpg',
+//     '../images/slider/slider-img-2.jpg'
+//   ];
+
+//   let currentSlideIndex = 0;
+
+//   const slider = document.querySelector('.slider');
+
+//   const updateSlide = () => {
+//     const currentSlide = slides[currentSlideIndex];
+//     slider.style.backgroundImage = `url(${currentSlide})`;
+//   };
+
+//   document.querySelector('.slider__button-next').addEventListener('click', (e) => {
+//     e.preventDefault();
+//     currentSlideIndex = (currentSlideIndex + 1) % slides.length;
+//     updateSlide();
+//   });
+
+//   document.querySelector('.slider__button-prev').addEventListener('click', (e) => {
+//     e.preventDefault();
+//     currentSlideIndex = (currentSlideIndex - 1 + slides.length) % slides.length;
+//     updateSlide();
+//   });
+
+//   // Инициализация первого слайда
+//   updateSlide();
+
+// });
