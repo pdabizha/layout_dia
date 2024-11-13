de3b4bd74e640e172cdebc1fe29dbd8f838aa4ab
Polina Dabizha <polinkadabi29@gmail.com>
2024-11-13 20:38:23 +0200

    make build

---
+++ index.5baf00dd.js
@@ -0,0 +1,2 @@
+document.addEventListener("input",function(t){t.target.classList.contains("input__textarea")&&(t.target.style.height="26px",t.target.style.height=`${t.target.scrollHeight}px`)});const t=document.querySelector(".page");document.querySelector(".theme-switcher").addEventListener("click",()=>{t.classList.toggle("theme--color")});
+//# sourceMappingURL=index.5baf00dd.js.map
